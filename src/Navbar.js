f74d63f03d2d1c3ef12bbb6c5c39f6dbf50d77f8
enPlace <n.place@protonmail.com>
2021-08-15 16:24:06 -0300

    Add basic Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="links">
+        <a href="/">Todo's</a>
+        <a href="/finished">Finished</a>
+        <a href="https://github.com/enPlace/react-todo-list">Github</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
